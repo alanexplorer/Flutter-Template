76237260b224c7013c567ad9d2bb33aad30485d7
Alan Pereira <contatosalan@outlook.com>
2020-10-10 12:32:11 -0300

    create, login, logout
    implementation function create user, login user and logout

---
+++ template_one/lib/screens/auth_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:template_one/models/auth_data.dart';
+import 'package:template_one/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool _isLoading = false;
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    AuthResult authResult;
+
+    try {
+      if (authData.isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+        final userData = {
+          'name': authData.name,
+          'email': authData.email,
+        };
+
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData(userData);
+      }
+    } on PlatformException catch (err) {
+      final msg = err.message ?? 'Ocorreu um erro! Verifique suas credenciais';
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                AuthForm(_handleSubmit),
+                if (_isLoading)
+                  Positioned.fill(
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5),
+                      ),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ template_one/lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Icon(
+                      Icons.exit_to_app,
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (item) {
+                if (item == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Seja Bem Vindo'),
+        ),
+      ),
+    );
+  }
+}
